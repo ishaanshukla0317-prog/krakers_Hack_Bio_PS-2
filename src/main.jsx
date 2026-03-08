17800217b7bb8eb804b03fcf2ae0da10111a239b
ishaanshukla0317-prog <ishaanshukla0317@gmail.com>
2026-03-09 01:49:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BacterialSimulationDashboard from '../bacterial_simulation_dashboard.jsx'
+import BacterialSimulationDashboard from './bacterial_simulation_dashboard.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
